e815eb16018deb83b1732c44368d2054b3034760
Isaias Bahena <bahei21@wfu.edu>
2026-05-16 13:16:51 -0500

    Clean up package initialization and versioning

---
+++ src/state_surfaces/__init__.py
@@ -0,0 +1,11 @@
+from .core import StateSurfaceResult, analyze
+from .pipeline import process_gauss_code, run_pipeline
+from .version import __version__
+
+__all__ = [
+    "StateSurfaceResult",
+    "analyze",
+    "process_gauss_code",
+    "run_pipeline",
+    "__version__",
+]
---
+++ src/state_surfaces/smoothing/__init__.py
@@ -0,0 +1,4 @@
+"""
+Smoothing routines for 1-gons, 2-gons and 3-gons
+used in the minimal genus state-surface algorithm.
+"""
